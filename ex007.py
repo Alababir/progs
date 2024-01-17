1ab9226c845b5d7dc38c61124cfe4409ad197519
Alababir <alababir8@gmail.com>
2024-01-17 15:25:49 -0300

    Criação e feito dos exercicios

---
+++ ex007.py
@@ -0,0 +1,4 @@
+c = float(input('Quanto você tem na carteira? R$'))
+d = c / 4.94
+e = c / 0.19
+print('Com R${:.2f} você consegue comprar {:.2f} Dolares e {:.2f} euros'.format(c, d, e))
---
+++ ex008.py
@@ -0,0 +1,5 @@
+a = float(input('Qual a altura da parede? '))
+l = float(input('Qual a largura da parede? '))
+lata = 2
+output = (a*l) / lata
+print('Sua parede de {}m de área ira usar {}l de tinta'.format((a*l), output))
